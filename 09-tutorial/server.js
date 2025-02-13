f02972d63c29265d1d8cf1c4d72042f8a8210dcc
Travis Stirling <travis@travisstirling.com>
2025-02-13 13:50:22 -0700

    feat: complete chapter 9 tutorial

--- 09-tutorial/server.js
+++ 09-tutorial/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const errorHandler = require('./middleware/errorHandler')
 const { logger } = require('./middleware/logEvents')
 const PORT = process.env.PORT || 3000
@@ -9,23 +10,10 @@ const PORT = process.env.PORT || 3000
 // custom middleware logger
 app.use(logger)
 
-// Cross Origin Resource Sharing
-const whitelist = ['https://www.google.com', 'http://localhost:3000']
-const corsOptions =  {
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
-    optionsSuccessStatus: 200
-}
+// cross origin resource sharing
 app.use(cors(corsOptions))
 
-// built-in middleware to handle urlencoded data
-// in other words, form data:
-// 'content-type: application/x-www-form-urlencoded"
+// built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }))
 
 // built-in middleware for json
@@ -33,19 +21,11 @@ app.use(express.json())
 
 // serve static files
 app.use(express.static(path.join(__dirname, '/public')))
-app.use('/subdir', express.static(path.join(__dirname, '/public')))
 
 // routes
 app.use('/', require('./routes/root'))
-app.use('/subdir', require('./routes/subdir'))
 app.use('/employees', require('./routes/api/employees'))
 
-app.use((req, res, next) => {
-    console.log(res.getHeaders())
-    next()
-})
-
-// app.use('/')
 app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')) {
